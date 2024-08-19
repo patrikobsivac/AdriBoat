5fc05d4f26ecb07ca2ea1fda37fbb072c7d328c4
Patrik Tovernić Obšivač <patriktovernic@gmail.com>
2024-08-19 21:16:05 +0200

    Početak implementacije početne stranice
    Radovi u tijeku...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <h1>Istaknuti plovila</h1>
+    <hr>
+    <br>
+    <v-row>
+      <v-col v-for="boat in boats">
+        <v-card
+          class="mx-auto"
+          max-width="350"
+          color="#e9e8eb"
+        >
+          <v-img
+            :src="/"
+            height="180px"
+            class="image-border"
+          ></v-img>
+
+
+          <v-card-subtitle>
+            Cijena: {{ formatThousands() }} €
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn
+              @click="setStates()"
+              color="red"
+              text
+            >
+              Zatražite informacije
+            </v-btn>
+			
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -17,4 +50,8 @@ export default {
 </script>
 
 <style scoped>
+.image-border {
+  border: 2px solid #000; 
+  border-radius: 5px;
+}
 </style>
