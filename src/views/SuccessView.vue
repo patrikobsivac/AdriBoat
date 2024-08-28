5999418e17fc75f98ba77f5b96203557af93dba8
Patrik Tovernić Obšivač <patriktovernic@gmail.com>
2024-08-28 22:24:41 +0200

    Create SuccessView.vue
    Work in progress

---
+++ src/views/SuccessView.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container class="text-center">
+      <v-card class="mx-auto" max-width="500">
+        <v-card-title>Success!</v-card-title>
+        <v-card-text>
+          <p>Vaš zahtjev je uspješno poslan.</p>
+          <p><strong>Vaš zahtjev kod:</strong></p>
+          <p>Sačuvajte ovaj kôd za buduću upotrebu.</p>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </template>
+  
+<script>
+
+</script>
